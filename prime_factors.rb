74d71f9391b535c6adffcfde56089ea52a26eb88
Hannie Dong <hdong92@gmail.com>
2018-01-10 18:16:05 -0800

    Create prime factor algorithm.

---
+++ prime_factors.rb
@@ -0,0 +1,20 @@
+def prime_factors(number)
+  prime_factors = []
+  counter = 2 
+  until number == 1 
+    if number % 2 == 0 
+      prime_factors << 2 
+      number /= 2 
+    end
+    if number % counter == 0 
+      prime_factors << counter 
+      number /= counter 
+    end 
+    if number / counter == 1 
+      prime_factors << counter
+      number /= counter
+    end 
+    counter += 1 
+  end 
+  prime_factors
+end 
