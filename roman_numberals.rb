4fb6754a4eb3e909bd466251b236cf78323126f3
Hannie Dong <hdong92@gmail.com>
2018-01-22 16:41:52 -0800

    Create more efficient numerals converter method.

--- roman_numberals.rb
+++ roman_numberals.rb
@@ -1,4 +1,6 @@
 def convert_to_roman(number, *modern)
+  roman_numeral_string = ''
+  
   roman_numerals = {
     1000 => "M",
     500 => "D",
@@ -19,22 +21,25 @@ def convert_to_roman(number, *modern)
     4 => "IV",
     1 => "I"
   }
-  roman_numeral_string = ''
-  if modern == true 
-    modern_roman_numerals.each do |key, value|
-      until number < key 
-        roman_numeral_string << value 
-        number -= key 
-      end 
-    end 
-    roman_numeral_string
+  
+  if modern == [true]
+    numerals_hash = modern_roman_numerals
   else
-    modern_roman_numerals.each do |key, value|
-      until number < key 
-        roman_numeral_string << value 
-        number -= key 
-      end 
-    end 
-    roman_numeral_string
+    numerals_hash = roman_numerals
   end
+  
+  numerals_hash.each do |value, character|
+    if number >= value && number > 0 
+      if number / value > 1 
+        number_of_characters = (number / value).floor
+      else
+        number_of_characters = 1
+      end 
+    roman_numeral_string << character * number_of_characters
+    number -= value * number_of_characters
+    end 
+  end
+roman_numeral_string
 end 
+
+convert_to_roman(4, true)
