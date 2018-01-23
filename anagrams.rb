1d8c5b0c3579d18af152e495ebfee9c61ac1d688
Hannie Dong <hdong92@gmail.com>
2018-01-22 20:42:28 -0800

    Create find anagram method.

--- anagrams.rb
+++ anagrams.rb
@@ -1,8 +1,20 @@
+
+
 def anagram_canonical_form(word)
-  word.split('').sort.join('')
+  word.downcase.split('').sort.join('')
   # Manipulate word into canonical form ...
 end
 
 def anagrams?(word, possible_anagram)
   anagram_canonical_form(word) == anagram_canonical_form(possible_anagram)
 end
+
+def find_anagrams(word, list)
+  anagram_array = []
+  list.each do |element|
+    if anagrams?(word, element) == true 
+      anagram_array << element 
+    end 
+  end 
+  anagram_array
+end 
