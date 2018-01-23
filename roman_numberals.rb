ce692b49f94e36fefa196893b57c3ef628047e95
Hannie Dong <hdong92@gmail.com>
2018-01-22 16:19:03 -0800

    Create modern roman numerals converter.

--- roman_numberals.rb
+++ roman_numberals.rb
@@ -1,5 +1,4 @@
-
-def convert_to_roman(number)
+def convert_to_roman(number, *modern)
   roman_numerals = {
     1000 => "M",
     500 => "D",
@@ -10,14 +9,32 @@ def convert_to_roman(number)
     1 => "I"
   }
 
+  modern_roman_numerals = {
+    1000 => "M",
+    500 => "D",
+    100 => "C",
+    50 => "L",
+    10 => "X",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+  }
   roman_numeral_string = ''
-  roman_numerals.each do |key, value|
-    until number < key 
-      roman_numeral_string << value 
-      number -= key 
+  if modern == true 
+    modern_roman_numerals.each do |key, value|
+      until number < key 
+        roman_numeral_string << value 
+        number -= key 
+      end 
     end 
-  end 
-  roman_numeral_string
+    roman_numeral_string
+  else
+    modern_roman_numerals.each do |key, value|
+      until number < key 
+        roman_numeral_string << value 
+        number -= key 
+      end 
+    end 
+    roman_numeral_string
+  end
 end 
-
-convert_to_roman(14)
