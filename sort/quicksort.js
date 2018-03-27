fb9b8cf36f5afc0e5e9b97ce13f8933794ba103e
Hannie Dong <hdong92@gmail.com>
2018-03-27 12:51:56 -0700

    Quick sort.

---
+++ sort/quicksort.js
@@ -0,0 +1,39 @@
+const quickySort = array => {
+    if (array.length === 0) return [];
+    let [x, ...xs] = array; 
+    return [
+        ...quickySort(xs.filter(y => y < x)),
+        x,
+        ...quickySort(xs.filter(y => y >= x))
+    ];
+};
+let array = [1,7,2,4]
+quickySort(array)
+
+
+function quickSort(array){
+  if(array.length ===0){
+    return [];
+  }
+  let pivot = array[Math.floor(Math.random()*array.length)];
+  let { lessThan, equalTo, greaterThan} = sortAtPivot(array, pivot)
+
+  return [...quickSort(lessThan), ...equalTo, ...quickSort(greaterThan)];
+}
+
+function sortAtPivot(array, pivot){
+  let lessThan = [];
+  let equalTo = [];
+  let greaterThan = [];
+
+  array.forEach(element=>{
+    if(element < pivot){
+      lessThan.push(element);
+    }else if (element === pivot){
+      equalTo.push(element);
+    }else{
+      greaterThan.push(element);
+    }
+  });
+  return {lessThan, equalTo, greaterThan};
+}
