6288781309ae898299d60e965186d7c0377e03c4
Hannie Dong <hdong92@gmail.com>
2018-03-31 14:45:12 -0700

    Create binary search algo

---
+++ binarySearch.js
@@ -0,0 +1,21 @@
+function binarySearch(array, value, minIndex = 0, maxIndex = array.length - 1){
+
+  let currentIndex
+
+  currentIndex = Math.floor((minIndex + maxIndex)/2);
+  if(array[currentIndex] === value){
+    console.log(currentIndex)
+    return;
+  }
+  if(value > array[currentIndex]){
+    minIndex = currentIndex + 1;
+  }
+  if(value < array[currentIndex]){
+    maxIndex = currentIndex - 1;
+  }
+  
+  binarySearch(array, value, minIndex, maxIndex)
+
+}
+
+binarySearch([1,2,3,4,5,6,7], 2)
