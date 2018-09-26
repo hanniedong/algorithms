e0be2ff9a005133b34a9152f5835c1cc48b0c202
Hannie Dong <hdong92@gmail.com>
2018-09-26 14:14:09 -0700

    Linked lists practice

--- linked_lists/doublyLinkedList.js
+++ linked_lists/doublyLinkedList.js
@@ -1,33 +1,97 @@
 class Node {
-  constructor(data){
-    this.data = data;
+  constructor(element) {
+    this.element = element;
     this.next = null;
     this.prev = null;
   }
 }
 
-class DoublyLinkedList{
-  constructor(){
+class DoublyLinkedList {
+  constructor() {
     this.count = 0;
     this.head = null;
     this.tail = null;
   }
 
-  get length(){
-    return this.count;
+  getElementAt(index) {
+    if (index >= 0 && index <= this.count) {
+      let current = this.head;
+      for (let i = 0; i < index; i++) {
+        current = current.next;
+      }
+      return current;
+    }
+    return undefined;
   }
 
-  addLast(data){
-    let node = new Node(data)
-    if(this.count){
+  removeAt(index) {
+    if ((index >= 0) & (index < this.count)) {
+      let current = this.head;
+      if (index === 0) {
+        this.head = current;
+        if (this.count === 1) {
+          this.tail = undefined;
+        } else {
+          this.head.prev = undefined;
+        }
+      } else if (index === this.count - 1) {
+        current = this.tail;
+        this.tail = current.prev;
+        this.tail.next = undefined;
+      } else {
+        current = this.getElementAt(index);
+        const previous = current.prev;
+        previous.next = current.next;
+        current.next.prev = previous;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
+
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(element);
+      let current = this.head;
+      if (index === 0) {
+        if (this.head === null) {
+          this.head = node;
+          this.tail = node;
+        } else {
+          node.next = this.head;
+          current.prev = node;
+          this.head = node;
+        }
+      } else if (index === this.count) {
+        current = this.tail;
+        current.next = node;
+        node.prev = current;
+        this.tail = node;
+      } else {
+        const previous = this.getElementAt(index);
+        current = previous.next;
+        node.next = current.next;
+        previous.next = node;
+        current.prev = node;
+        node.prev = previous;
+      }
+      this.count++;
+      return true;
+    }
+    return false;
+  }
+
+  addLast(data) {
+    let node = new Node(data);
+    if (this.count) {
       this.tail.next = node;
       node.previous = this.tail;
       this.tail = node;
-    }else{
+    } else {
       this.head = node;
       this.tail = node;
     }
-    this.count++
+    this.count++;
   }
-
 }
--- linked_lists/linkedList1.js
+++ linked_lists/linkedList1.js
@@ -1,1 +1,87 @@
-linkedList1.js
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.count = 0;
+  }
+
+  push(element) {
+    const node = new Node(element);
+    let current;
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.count++;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return this.count === 0;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
+
+  getElementAt(index) {
+    if (index >= 0 && index <= this.count) {
+      let current = this.head;
+      for (let i = 0; i < index; i++) {
+        current = current.next;
+      }
+      return current;
+    }
+    return undefined;
+  }
+
+  insert(element, index) {
+    if (index >= 0 && index < this.count) {
+      const node = new Node(element);
+      if (index === 0) {
+        const current = this.head;
+        node.next = current;
+        this.head = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        const current = previous.next;
+        node.next = current;
+        previous.next = node;
+      }
+      this.count++;
+      return true;
+    }
+    return false;
+  }
+}
